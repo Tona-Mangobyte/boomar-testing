d2acb050fe2d64add44a4d29fdadc5723703701a
Tona <tona@mango-byte.com>
2022-07-03 11:21:01 +0700

    test | locator

--- testing/home-page.spec.ts
+++ testing/home-page.spec.ts
@@ -9,5 +9,17 @@ test.describe('feature boomar', () => {
   test('go to home', async ({ page }) => {
     // Assertions use the expect API.
     await expect(page).toHaveURL('https://sb-www.boomar.games/');
+
+    const title = page.locator('.l-header__sitename__link');
+    await expect(title).toHaveText('Boomar');
+
+    // await expect(await page.locator('ul > li').count()).toBe(13);
+    // await expect(await page.locator('.l-header__nav')).toHaveText(['Book', 'News'])
+    // await expect(await page.locator('.l-header__nav').count()).toBe(1);
+    // await expect(await page.locator('.l-header__nav')).toContainText('Book')
+
+    await expect(await page.locator('ul > li').first()).toHaveText('Login');
+    await expect(page.locator('.l-header__nav__list .cap_menu').first()).toHaveText('book');
+    await expect(page.locator('.l-header__nav__list .cap_menu').nth(1)).toHaveText('news');
   });
 });
