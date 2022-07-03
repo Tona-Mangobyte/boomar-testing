91615f09841be943f36605f320c3f1d013a7649f
Tona <tona@mango-byte.com>
2022-07-03 15:48:56 +0700

    test login page /e2e

--- testing/home-page.spec.ts
+++ testing/home-page.spec.ts
@@ -1,14 +1,15 @@
 import { test, expect } from '@playwright/test';
+require('dotenv').config()
 
 test.describe('feature boomar', () => {
   test.beforeEach(async ({ page }) => {
     // Go to the starting url before each test.
-    await page.goto('https://sb-www.boomar.games/');
+    await page.goto(process.env.BASE_URL);
   });
 
   test('go to home', async ({ page }) => {
     // Assertions use the expect API.
-    await expect(page).toHaveURL('https://sb-www.boomar.games/');
+    await expect(page).toHaveURL(process.env.BASE_URL);
 
     const title = page.locator('.l-header__sitename__link');
     await expect(title).toHaveText('Boomar');
@@ -21,5 +22,26 @@ test.describe('feature boomar', () => {
     await expect(await page.locator('ul > li').first()).toHaveText('Login');
     await expect(page.locator('.l-header__nav__list .cap_menu').first()).toHaveText('book');
     await expect(page.locator('.l-header__nav__list .cap_menu').nth(1)).toHaveText('news');
+
+    /*const locator = page.locator('.l-header__login');
+    await locator.click();*/
+    // await expect(await page.locator('.c-form__submit')).toHaveText('Login');
+    // await expect(await page.locator('button', { hasText: 'Login' })).toHaveText('Login');
+    // await expect(await page.locator("input[name='identifier']")).toHaveText('tona@mango-byte.com');
+
+  });
+
+  test('go to login', async ({ page }) => {
+    await page.goto(`${process.env.BASE_URL}/login/`);
+
+    await expect(page).toHaveURL(`${process.env.BASE_URL}/login/`);
+
+    await page.fill("input[name='identifier']", 'tona@mango-byte.com')
+    await page.fill("input[name='password']", 'MB@q1w2e3r4t5');
+    await page.click('text=Login')
+
+    await page.goto(process.env.BASE_URL);
+    const title = page.locator('.l-header__sitename__link');
+    await expect(title).toHaveText('Boomar');
   });
 });
