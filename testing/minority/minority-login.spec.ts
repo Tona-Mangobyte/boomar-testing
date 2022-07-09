34345a854fc80bf301b31fcbf3496e0af7647cf6
Tona <tona@mango-byte.com>
2022-07-09 18:49:12 +0700

    add web.testing minority

---
+++ testing/minority/minority-login.spec.ts
@@ -0,0 +1,47 @@
+import {expect, test} from "@playwright/test";
+
+test.describe('feature login in minority', () => {
+
+    test('go to login page', async ({ page }) => {
+        await page.goto(`${process.env.BASE_URL_MINORITY}/login/`)
+        await page.fill("input[name='email']", process.env.USER_NAME_MINORITY)
+        await page.fill("input[name='password']", process.env.PASSWORD_MINORITY)
+
+        // await expect(page.locator("input[name='email']")).toHaveText(process.env.USER_NAME_MINORITY)
+        // await expect(page.locator("input[name='password']")).toHaveText(process.env.PASSWORD_MINORITY)
+
+        await page.locator(".v-btn__content").click()
+        await expect(page).toHaveURL(`${process.env.BASE_URL_MINORITY}/users`);
+
+        await page.goto(`${process.env.BASE_URL_MINORITY}/quick`)
+        // await expect(page).toHaveURL(`${process.env.BASE_URL_MINORITY}/quick`);
+
+        await page.goto(`${process.env.BASE_URL_MINORITY}/configuration`)
+        await expect(page).toHaveURL(`${process.env.BASE_URL_MINORITY}/configuration`);
+    });
+
+    /*test('go to login page and go to quick page', async ({ page }) => {
+        // const page = await context.newPage();
+        await page.goto(`${process.env.BASE_URL_MINORITY}/login/`)
+        await page.fill("input[name='email']", process.env.USER_NAME_MINORITY)
+        await page.fill("input[name='password']", process.env.PASSWORD_MINORITY)
+
+        await page.locator(".v-btn__content").click()
+        await expect(page).toHaveURL(`${process.env.BASE_URL_MINORITY}/users`);
+
+        await page.goto(`${process.env.BASE_URL_MINORITY}/quick`)
+        await expect(page).toHaveURL(`${process.env.BASE_URL_MINORITY}/quick`);
+    });
+
+    test('go to login page and go to configuration page', async ({ page }) => {
+        await page.goto(`${process.env.BASE_URL_MINORITY}/login/`)
+        await page.fill("input[name='email']", process.env.USER_NAME_MINORITY)
+        await page.fill("input[name='password']", process.env.PASSWORD_MINORITY)
+
+        await page.locator(".v-btn__content").click()
+        await expect(page).toHaveURL(`${process.env.BASE_URL_MINORITY}/users`);
+
+        await page.goto(`${process.env.BASE_URL_MINORITY}/configuration`)
+        await expect(page).toHaveURL(`${process.env.BASE_URL_MINORITY}/configuration`);
+    });*/
+});
---
+++ testing/minority/playwright.config.ts
@@ -0,0 +1,19 @@
+import { PlaywrightTestConfig, devices } from '@playwright/test';
+require('dotenv').config()
+const config: PlaywrightTestConfig = {
+  use: {
+    baseURL: 'http://localhost:3000',
+    browserName: 'chromium',
+    headless: false,
+    viewport: { width: 1280, height: 720 },
+    ignoreHTTPSErrors: true,
+    video: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+};
+export default config;
